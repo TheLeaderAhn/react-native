cfe87f75457870bd5f0b9399617be6effab5b9fd
TheLeaderAhn <lux-veritas@naver.com>
2024-05-02 14:16:27 +0900

    변수사용및  환경구성 후 테스트

--- myfirstRn/App.js
+++ myfirstRn/App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log(" Expo React Native init....");
+
+  //java Script 사용하긱
+  const name = "jae young";
+  const isFullname = true;
+
+  const add = (a, b) => {
+    return a + b;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>내 첫 리액트 네이티브 프로젝트!</Text>
+      <Text>변수 사용</Text>
+      <Text>myFullName : {isFullname ? name + " Ahn" : name}</Text>
+
+      <Text>1 + 2 = {add(1, 2)}</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
